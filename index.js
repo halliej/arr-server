88d3f4ea7a0361cb7d569424a078ca9a599d2414
Hal DeLage <hal.delage@teamaol.com>
2017-01-11 16:53:06 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+//const http = require('http');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost:auth/auth');
+
+const app = express();
+const router = require('./router');
+
+app.use(morgan('combined'));
+app.use(bodyParser.json({ type: '*/*' }));
+router(app);
+
+const port = process.env.PORT || 3090;
+app.listen(port, () => {
+  console.log(`Running on port ${port}`);
+});
+
+module.exports = app;
